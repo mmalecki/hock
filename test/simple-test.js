374ff7832f9bf042f1f9f0c5f5ea623f86ef9b06
Maciej Małecki <me@mmalecki.com>
2013-04-30 17:50:51 +0200

    [test] Moar tests

--- test/simple-test.js
+++ test/simple-test.js
@@ -1,4 +1,5 @@
 var assert = require('assert'),
+    async = require('async'),
     request = require('request'),
     cb = require('assert-called'),
     hock = require('../');
@@ -8,12 +9,32 @@ var PORT = 5678;
 var mock = hock(PORT, cb(function () {
   mock
     .get('/url')
-      .reply(200, { 'hock': 'ok' });
+      .reply(200, { 'hock': 'ok' })
+    .post('/post', { 'hock': 'post' })
+      .reply(201, { 'hock': 'created' });
 
-  request('http://localhost:' + PORT + '/url', cb(function (err, res, body) {
-    assert(!err);
-    assert.equal(res.statusCode, 200);
-    assert.deepEqual(JSON.parse(body), { 'hock': 'ok' });
+  async.parallel([
+    function (done) {
+      request('http://localhost:' + PORT + '/url', cb(function (err, res, body) {
+        assert(!err);
+        assert.equal(res.statusCode, 200);
+        assert.deepEqual(JSON.parse(body), { 'hock': 'ok' });
+        done();
+      }));
+    },
+    function (done) {
+      request({
+        method: 'POST',
+        url: 'http://localhost:' + PORT + '/post',
+        json: { 'hock': 'post' }
+      }, cb(function (err, res, body) {
+        assert(!err);
+        assert.equal(res.statusCode, 201);
+        assert.deepEqual(body, { 'hock': 'created' });
+        done();
+      }));
+    }
+  ], function () {
     mock.close();
-  }));
+  });
 }));
